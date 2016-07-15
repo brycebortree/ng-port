acaf247fade8e6038cddbb107cdc85a252cf9aba
brytree <brycebortree@gmail.com>
2016-07-14 21:26:54 -0700

    reloadable page

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/assets', express.static(__dirname + '/public/assets'));
+app.use('/views', express.static(__dirname + '/public/views'));
+
+app.all('/*', function(req, res, next) {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
 
 app.listen(process.env.PORT || 3000);
