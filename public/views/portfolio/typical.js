ed27602c86c4bd3781d0db8a0382cd039e708247
brytree <brycebortree@gmail.com>
2016-07-26 12:10:42 -0700

    refactoring code

--- public/views/portfolio/typical.js
+++
@@ -1,9 +0,0 @@
-console.log('hello'.repeatify(3));
-
-String.prototype.repeatify = String.prototype.repeatify || function(times) {
-  var str = '';
-  for(var i = 0; i < times; i++){
-    str += this;
-  }
-  return str;
-}
